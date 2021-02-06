5d03af224e036cf5c1d2200bdb53381bace2e674
Mika <>
2021-02-06 18:07:41 +0100

    extend nextjs config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 
 module.exports = {
-  targets: "serverless",
+  target: "serverless",
   webpack: config => {
     const env = {API_KEY: apiKey};
     config.plugins.push(new webpack.DefinePlugin(env));
