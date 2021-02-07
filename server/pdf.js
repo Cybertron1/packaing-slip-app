2651b77871a141e9600de890e0b6490972ca13a0
Mika <>
2021-02-07 10:59:13 +0100

    add heap stuff

--- server/pdf.js
+++ server/pdf.js
@@ -2,8 +2,8 @@ import pdf from 'html-pdf';
 import fs from 'fs';
 import {compile} from 'handlebars';
 
-const html = fs.readFileSync('./template/template.html', 'utf8');
 const createPdf = (orders) => {
+  const html = fs.readFileSync('./template/template.html', 'utf8');
   const template = compile(html);
   const htmlOrders = template(orders);
   const options = {
