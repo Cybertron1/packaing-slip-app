2218d129e4edbfb57e6c835737f97b30f80684b8
Mika <>
2021-02-14 17:17:25 +0100

    add mongodb uri

--- pages/api/_middleware/database.js
+++ pages/api/_middleware/database.js
@@ -4,7 +4,7 @@ import nc from 'next-connect';
 export default nc()
   .use(async (req, res, next) => {
     if (mongoose.connection.readyState === 0) {
-      await mongoose.connect('mongodb://localhost:27017/slip', {
+      await mongoose.connect(process.env.MONGO_DB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false
